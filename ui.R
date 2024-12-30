5e270bd0b155bf79da92757c66fe60c9f22d912f
Katie Harrington <knharrington@mote.org>
2024-12-30 14:06:30 -0500

    add input for species

--- ui.R
+++ ui.R
@@ -8,16 +8,26 @@ fluidPage(
     sidebarPanel(
       h4("About the app"),
       helpText("help text example"),
+      selectInput("filter_species", label="Select Species", choices = sort(unique()), selected = "Sandbar Shark",
+                  selectize = TRUE, multiple=TRUE),
       sliderInput()
     ), # sidebarPanel
     
     mainPanel(
       wellPanel(
         fluidRow(
-          column(),
-          column(),
-          column()
+          column(
+            width = 4,
+            selectInput("select_species", label="Select Species", choices = sort(species_list)),
+          ),
+          column(
+            width = 4,
+          ),
+          column(
+            width = 4,
+          )
         ), # fluidRow
+        br(),
         fluidRow(
           column(),
           column(),
